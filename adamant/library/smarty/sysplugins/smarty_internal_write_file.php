10f53eb9b08d39b7475d9e1f1709122dcde19745
Philipp Tkachev <zoonman@gmail.com>
2013-11-09 19:42:08 -0500

    Smarty errors fixes

--- adamant/library/smarty/sysplugins/smarty_internal_write_file.php
+++ adamant/library/smarty/sysplugins/smarty_internal_write_file.php
@@ -55,7 +55,7 @@ class Smarty_Internal_Write_File
          */
         if (Smarty::$_IS_WINDOWS) {
             // remove original file
-            @unlink($_filepath);
+            if(file_exists($_filepath)) @unlink($_filepath);
             // rename tmp file
             $success = @rename($_tmp_file, $_filepath);
         } else {
@@ -63,7 +63,7 @@ class Smarty_Internal_Write_File
             $success = @rename($_tmp_file, $_filepath);
             if (!$success) {
                 // remove original file
-                @unlink($_filepath);
+                if(file_exists($_filepath)) @unlink($_filepath);
                 // rename tmp file
                 $success = @rename($_tmp_file, $_filepath);
             }
