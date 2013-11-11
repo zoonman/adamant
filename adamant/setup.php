7a2f3d616263ddf6cbaa3ec65b89b06976898393
Philipp Tkachev <zoonman@gmail.com>
2013-11-11 00:51:38 -0500

    memcached config

--- adamant/setup.php
+++ adamant/setup.php
@@ -28,6 +28,12 @@ CONFIG_T;
 if (setup_validate_form_data()) {
 	
 	$config_file_content = str_replace(array('{db}','{host}','{user}','{pass}'), array($_POST['db'],$_POST['host'],$_POST['user'],$_POST['pass']), $config_file_template);
+	
+	if (isset($_POST['memcached_port']) && $_POST['memcached_port'] > 0
+		&& isset($_POST['memcached_host']) && !empty($_POST['memcached_host'])
+	) {
+		$config_file_content .= "\n define('MEMCACHED_HOST', '".$_POST['memcached_host']."'); \n define('MEMCACHED_PORT', '".$_POST['memcached_port']."'); \n";
+	}
 	if (@file_put_contents('adamant/config.php', $config_file_content) != 0) {
 		echo '<div class="message success">Config installed. <a href="/">Continue</a>.</div>';
 	}
@@ -49,6 +55,9 @@ function setup_validate_form_data() {
 			<p><label for="db">Database name</label><input type="text" name="db" id="db" value="adamant">
 			<p><label for="user">Username</label><input type="text" name="user" id="user" value="username">
 			<p><label for="pass">Password</label><input type="text" name="pass" id="pass" placeholder="password">
+			<p>If you want to use memcached:</p>
+			<p><label for="memcached_host">Memcached Host</label><input type="text" name="memcached_host" id="memcached_host" placeholder="localhost">
+			<p><label for="memcached_port">Memcached Portt</label><input type="text" name="memcached_port" id="memcached_port" placeholder="11211">
 			<p><input type="submit">
 		</form>
 SETUP_FORM;
